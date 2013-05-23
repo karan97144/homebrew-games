8befe45fc901ea964770e7abcdc3df1f48506ce2
Justin de Vesine <ziz@ziz.org>
2013-05-23 11:23:45 -0600

    glulxe: add HEAD

--- glulxe.rb
+++ glulxe.rb
@@ -5,6 +5,7 @@ class Glkterm < Formula
   url 'http://www.eblong.com/zarf/glk/glkterm-104.tar.gz'
   version '1.0.4'
   sha1 'cf67bc8f93e6c71898f59e8083fc65622ed02d54'
+  head 'https://github.com/erkyrath/glkterm.git'
 end
 
 class Glulxe < Formula
@@ -12,11 +13,12 @@ class Glulxe < Formula
   url 'http://www.eblong.com/zarf/glulx/glulxe-050.tar.gz'
   version '0.5.0'
   sha1 '2b6a8c4494f712667cc2aa4147a49842997c3696'
+  head 'https://github.com/erkyrath/glulxe.git'
 
   def install
     glkterm_lib = libexec+'glkterm/lib'
     glkterm_include = libexec+'glkterm/include'
-    Glkterm.new.brew do
+    Glkterm.new('glulxe__glkterm').brew do
       system 'make'
       glkterm_lib.install 'libglkterm.a'
       glkterm_include.install 'glk.h', 'glkstart.h', 'gi_blorb.h', 'gi_dispa.h', 'Make.glkterm'
